3b6d9ce387f4a3a33d76dbd185cbe4ed4ad5523d
tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2023-07-30 16:32:17 +0200

    "about me page modified"

--- personal.js
+++ personal.js
@@ -5,7 +5,7 @@ document.addEventListener("alpine:init",()=>{
             fullName:'Samuel Tiisetso Pudinyane',
             aboutMe_p1:"I am Samuel Tiisetso Pudinyane from Free State province, with Bsc in software engineering. Studied at Bahcesehir university in Turkey through scholarship opportunity from free state government. i am currently looking for an internship to complete my training as a requirements from the university.",
             aboutMe_p2:"I firmly believe that if given the opportunity it would enable me to access to the new adventures. I'm keen to explore, develop and gain skills. Obtained certificates from microsoft on Data fundamentals, azure cloud fundamentals, azure AI fundamentals as well as FreeCodeCamp JavaScript. ",
-            aboutMe_p3:"indivudually I am a very proactive person, person of multiple solutions to one problem,very analytical when it comes to assessing risks before applying the solution. I have a good communication skills and good reputation when working in collaboration and as a team member, I am an outspoken person with a good critical thinking skill and always aiming for good results.",
+            aboutMe_p3:"Individually I am a very proactive person, person of multiple solutions to one problem, very analytical when it comes to assessing risks before applying the solution. I have a good communication skills and good reputation when working in collaboration and as a team member, I am an outspoken person with a good critical thinking skill and always aiming for good results.",
             sacArrayList:[],
             contacts(){
                 return window.location.href = "personal-contacts.html";
